16d4406eea818bfe86683d31d680e4c51f218c2f
0147123 <9536442@gmail.com>
2021-12-29 05:00:14 +0800

    update

---
+++ src/DataStore.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class DataStore {
+    private double desire_cgpa;
+    private double currCgpa;
+    private int completeCred;
+    private int remainCred;
+
+    private boolean commonCheck(String buffer){
+
+        //Validation check - null input
+        //if user input null value, user need to re-enter the value
+        if(buffer.isEmpty()){
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkCgpa(String buffer, int id) {
+        double douTemp;
+
+        if (!this.commonCheck(buffer))
+            return false;
+
+
+        //validation check - data type
+        //if user input wrong data type, user need to re-enter the value
+        try{
+            douTemp = Double.parseDouble(buffer);    //convert string type to integer type
+        }
+        catch (NumberFormatException ex){
+            return false;
+        }
+
+        //validation check - number range
+        //the input should larger than 0
+        if ((douTemp >= 0) && (douTemp <= 4.3)) {
+            switch (id){
+                case 1:
+                    desire_cgpa = douTemp;
+                    break;
+                case 2:
+                    currCgpa = douTemp;
+                    break;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkCred(String buffer, int id){
+        int intTemp;
+        if (!this.commonCheck(buffer))
+            return false;
+
+        //validation check - data type
+        //if user input wrong data type, user need to re-enter the value
+        try{
+            intTemp = Integer.parseInt(buffer);    //convert string type to integer type
+        }
+        catch (NumberFormatException ex){
+            return false;
+        }
+
+        //validation check - number range
+        //the input should larger than 0
+        if ((intTemp >= 0) && (intTemp <= 100)){
+            switch (id){
+                case 1:
+                    completeCred = intTemp;
+                    break;
+                case 2:
+                    remainCred = intTemp;
+                    break;
+            }
+            return true;
+        }
+        else{
+            System.out.println("\tInvalid input: please input a suitable range!");
+            return false;
+        }
+    }
+
+    public double getCurrCgpa() {
+        return currCgpa;
+    }
+
+    public double getDesire_cgpa() {
+        return desire_cgpa;
+    }
+
+    public int getCompleteCred(){
+        return completeCred;
+    }
+
+    public int getRemainCred() {
+        return remainCred;
+    }
+}
--- src/GUI.java
+++ src/GUI.java
@@ -1,102 +1,173 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+
+import java.awt.event.*;
+
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.RoundRectangle2D;
 
 public class GUI{
     private JFrame frame;
-    private JPanel panel;
-    private JLabel desireCgpaLabel;
-    private JLabel currCgpaLabel;
-    private JLabel completeCredLabel;
-    private JLabel remainCredLabel;
-    private JTextField desireCgpaTField;
-    private JTextField currCgpaTField;
-    private JTextField completeCredTField;
-    private JTextField remainCredTField;
-    private JButton calculateButton;
     private DataStore validation;
+
+    
+	private JPanel contentPane;
+	private JTextField inputTextField [];
+    private JLabel displayLabel [];
+    private Point initialClick;
 
 
     public GUI(){
         validation = new DataStore();
-        panel = new JPanel();
         frame = new JFrame();
 
+        frame.setUndecorated(true);
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+             public void componentResized(ComponentEvent e) {
+                frame.setShape(new RoundRectangle2D.Double(0, 0, frame.getWidth(), frame.getHeight(), 15, 15));
+             }
+         });
         frame.setSize(420, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //this method should call after using setSize() method!!
         frame.setLocationRelativeTo(null);
-        frame.add(panel);
-        frame.setResizable(false);
-
-        panel.setLayout(null);
-
-        //desire Cgpa
-        desireCgpaLabel = new JLabel("Your Desire cgpa");
-        desireCgpaLabel.setBounds(10, 15, 130, 25);
-        panel.add(desireCgpaLabel);
-
-        desireCgpaTField = new JTextField("pls enter in range 0-4.3");
-        desireCgpaTField.setBounds(150, 15, 200, 25);
-        panel.add(desireCgpaTField);
 
 
-        //current Cgpa
-        currCgpaLabel = new JLabel("Your current cgpa");
-        currCgpaLabel.setBounds(10, 50, 130, 25);
-        panel.add(currCgpaLabel);
+		contentPane = new JPanel();
+		contentPane.setBorder(null);
+		contentPane.setPreferredSize(new Dimension(440, 340));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setOpaque(false);
+		frame.setContentPane(contentPane);
+		
+		JPanel exitPanel = new JPanel();
+		exitPanel.setBackground(Color.decode("#233E6F"));
+		exitPanel.setPreferredSize(new Dimension(440, 35));
+		exitPanel.setLayout(null);
+		contentPane.add(exitPanel, BorderLayout.NORTH);
+		
+        ImageIcon closeimage = new ImageIcon("close.png");
+		JButton closeButton = new JButton("", closeimage);
+		closeButton.setRolloverIcon(closeimage);
+		closeButton.setBounds(398, 2, 30, 30);
+		closeButton.setBackground(Color.decode("#233E6F"));
+        closeButton.setBorderPainted(false);
+        closeButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		exitPanel.add(closeButton);
+		
+		JPanel DataPanel = new JPanel();
+		DataPanel.setPreferredSize(new Dimension(440, 250));
+		DataPanel.setBackground(Color.decode("#5F8ACB"));
+		contentPane.add(DataPanel, BorderLayout.CENTER);
+		DataPanel.setLayout(null);
+		
+		displayLabel = new JLabel[4];
+		displayLabel[0] = new JLabel("Current CGPA");
+		displayLabel[1] = new JLabel("Desire CGPA");
+		displayLabel[2] = new JLabel("Credits earned");
+		displayLabel[3] = new JLabel("Remaining credit");
 
-        currCgpaTField = new JTextField("pls enter in range 0-4.3");
-        currCgpaTField.setBounds(150, 50, 200, 25);
-        panel.add(currCgpaTField);
+		inputTextField = new JTextField[4];
 
-        //completed Credit
-        completeCredLabel = new JLabel("Your completed credit");
-        completeCredLabel.setBounds(10, 85, 130, 25);
-        panel.add(completeCredLabel);
 
-        completeCredTField = new JTextField("please input a suitable credit range");
-        completeCredTField.setBounds(150, 85, 200, 25);
-        panel.add(completeCredTField);
 
-        //remaining Credit
-        remainCredLabel = new JLabel("Your remaining credit");
-        remainCredLabel.setBounds(10, 120, 130, 25);
-        panel.add(remainCredLabel);
+		//setting format
+		for (int i = 0; i <=3; i++) {
+			//set format for displayLabel
+			displayLabel[i].setForeground(Color.WHITE);
+			displayLabel[i].setFont(new Font("Arial", Font.PLAIN, 20));
+			displayLabel[i].setBounds(40, 26+i*58, 158, 24);
+			DataPanel.add(displayLabel[i]);
 
-        remainCredTField = new JTextField("please input a suitable credit range");
-        remainCredTField.setBounds(150, 120, 200, 25);
-        panel.add(remainCredTField);
+			//set format for inputTextField
+			inputTextField[i] = new RoundTextfield("Suitable Credit Range");
+			inputTextField[i].setOpaque(false);
+			inputTextField[i].setBounds(199, 24+i*58, 200, 25);
+			inputTextField[i].setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 0));
+            inputTextField[i].setFont(new Font("Arial", Font.PLAIN, 16));
+			inputTextField[i].setBackground(Color.decode("#BECFE8"));
+			DataPanel.add(inputTextField[i]);
+			inputTextField[i].setColumns(10);
+		}
 
-        calculateButton = new JButton("Calculate");
-        calculateButton.setBounds(10, 155, 100, 25);
-        panel.add(calculateButton);
 
+		
+		JPanel ComfirmPanel = new JPanel();
+		ComfirmPanel.setLayout(null);
+		ComfirmPanel.setPreferredSize(new Dimension(440, 55));
+		ComfirmPanel.setBackground(Color.decode("#5F8ACB"));
+		contentPane.add(ComfirmPanel, BorderLayout.SOUTH);
+		
+		JButton calButton = new CalButton();
+		calButton.setText("Calculate");
+		calButton.setOpaque(false);
+		calButton.setBackground(new Color(35, 62, 111, 170));
+		calButton.setForeground(Color.WHITE);
+		calButton.setFont(new Font("Arial", Font.PLAIN, 14));
+		calButton.setBounds(312, 2, 100, 30);
+		calButton.setBorderPainted(false);
+		ComfirmPanel.add(calButton);
 
         //register event handlers
         Handler handler = new Handler();
-        desireCgpaTField.addActionListener(handler);
-        currCgpaTField.addActionListener(handler);
-        completeCredTField.addActionListener(handler);
-        remainCredTField.addActionListener(handler);
-        calculateButton.addActionListener(handler);
+      
+        calButton.addActionListener(handler);
+
+        for (JTextField i : inputTextField){
+            i.addActionListener(handler);
+        }
 
         //register event handlers
-        desireCgpaTField.addFocusListener(new defaultText("pls enter in range 0-4.3"));
-        currCgpaTField.addFocusListener(new defaultText("pls enter in range 0-4.3"));
-        completeCredTField.addFocusListener(new defaultText("please input a suitable credit range"));
-        remainCredTField.addFocusListener(new defaultText("please input a suitable credit range"));
+        inputTextField[0].addFocusListener(new defaultText("Range: 0-4.3"));
+        inputTextField[1].addFocusListener(new defaultText("Range: 0-4.3"));
+        inputTextField[2].addFocusListener(new defaultText("Suitable Credit Range"));
+        inputTextField[3].addFocusListener(new defaultText("Suitable Credit Range"));
 
+        closeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+
+        exitPanel.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                initialClick = e.getPoint();
+                exitPanel.getComponentAt(initialClick);
+            }
+        });
+    
+        exitPanel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+    
+                // get location of Window
+                int thisX = frame.getLocation().x;
+                int thisY = frame.getLocation().y;
+    
+                // Determine how much the mouse moved since the initial click
+                int xMoved = e.getX() - initialClick.x;
+                int yMoved = e.getY() - initialClick.y;
+    
+                // Move window to this position
+                int X = thisX + xMoved;
+                int Y = thisY + yMoved;
+                frame.setLocation(X, Y);
+            }
+        });
+
+        frame.pack();
         frame.setVisible(true);
+
     }
 
     private class Handler implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
-            if(validation.checkCgpa(desireCgpaTField.getText(), 1) && validation.checkCgpa(currCgpaTField.getText(), 2) && validation.checkCred(completeCredTField.getText(), 1) && validation.checkCred(remainCredTField.getText(), 2)){
+            if(validation.checkCgpa(inputTextField[0].getText(), 1) && validation.checkCgpa(inputTextField[1].getText(), 2) 
+               && validation.checkCred(inputTextField[2].getText(), 1) && validation.checkCred(inputTextField[3].getText(), 2)){
                 Calculate calculate = new Calculate(validation);
                 JOptionPane.showConfirmDialog(null, calculate.tar(), "Calculate Result", 2);
             }
@@ -113,51 +184,78 @@ public class GUI{
         public defaultText(String hint){
             this.hint = hint;
             isModify = false;
+
         }
 
         @Override
         public void focusGained(FocusEvent e) {
-            if(e.getSource() == currCgpaTField && !isModify) {
-                currCgpaTField.setText("");
+            if (e.getSource() instanceof JTextField) {
+                JTextField textField = (JTextField) e.getSource();
+                if(!isModify) 
+                    textField.setText("");
             }
-            else if(e.getSource() == desireCgpaTField && !isModify)
-                desireCgpaTField.setText("");
-            else if(e.getSource() == completeCredTField && !isModify)
-                completeCredTField.setText("");
-            else if(e.getSource() == remainCredTField && !isModify)
-                remainCredTField.setText("");
         }
 
         @Override
         public void focusLost(FocusEvent e) {
-            if(e.getSource() == currCgpaTField) {
-                if(currCgpaTField.getText().isEmpty())
-                    currCgpaTField.setText(hint);
+            if (e.getSource() instanceof JTextField) {
+                JTextField textField = (JTextField) e.getSource();
+                if(textField.getText().isEmpty())
+                    textField.setText(hint);
+                else if (textField.getText().equals(hint))
+                    isModify = false;
                 else
                     isModify = true;
             }
-            else if(e.getSource() == desireCgpaTField) {
-                if(desireCgpaTField.getText().isEmpty())
-                    desireCgpaTField.setText(hint);
-                else
-                    isModify = true;
-            }
-            else if(e.getSource() == completeCredTField) {
-                if (completeCredTField.getText().isEmpty())
-                    completeCredTField.setText(hint);
-                else
-                    isModify = true;
-            }
-            else if(e.getSource() == remainCredTField) {
-                if (remainCredTField.getText().isEmpty())
-                    remainCredTField.setText(hint);
-                else
-                    isModify = true;
-            }
-        }
 
+        }
 
     }
 
+	private class RoundTextfield extends JTextField{
+	    private Shape shape;
+
+        public RoundTextfield(String title){
+            super(title);
+        }
+	
+	    protected void paintComponent(Graphics g) {
+			Graphics2D graphics = (Graphics2D) g;
+			//
+			graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+			graphics.setColor(new Color(0, 0, 0, 100));
+			graphics.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);// arc Dimension
+
+			graphics.setColor(getBackground());
+	        graphics.fillRoundRect(0, 0, getWidth(), getHeight()-3, 5, 5);
+	        super.paintComponent(g);
+	    }
+
+	}
+	
+	private class CalButton extends JButton{
+		@Override
+	    protected void paintComponent(Graphics g) {
+			g.setColor(getBackground());
+	        g.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+	        super.paintComponent(g);
+	    }
+
+	    protected void paintBorder(Graphics g) {
+	        g.setColor(new Color(0, 0, 0, 0));
+	        g.drawRoundRect(0, 0, getWidth(), getHeight(), 10, 10); 
+	    }
+
+	    Shape shape;
+	    public boolean contains(int x, int y) {
+	        if (shape == null || 
+	                !shape.getBounds().equals(getBounds())) {
+	            shape = new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 10, 10);
+	        }
+	        return shape.contains(x, y);
+	    }
+
+	}
 
 }
